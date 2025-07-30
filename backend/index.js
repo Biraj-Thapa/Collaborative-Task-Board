86ca67114d7525bacb0f3b6018a2c7e80872384b
Biraj Thapa <birajthapa983@gmail.com>
2025-07-30 16:34:38 +0545

    login and register created

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import 'dotenv/config'
 import dbConnect from "./db/connection.js";
+import userRouter from "./routes/user.route.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const port = process.env.PORT || 6000;
 
-app.get("/", (req, res) => {
-  res.send("I am fine");
-});
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api",userRouter)
 
 
 app.listen(port,() => {
